694fc05e3c383d998b07484d7e7083ca2f4fd6d3
Jeff Zheng <121210783+Jsz2001@users.noreply.github.com>
2023-01-15 04:04:04 -0600

    Updated class to utilize lists for data
    replaced only storing the first and last datapoint with storing all data point within the two datapoints within a list

--- stock_class.py
+++ stock_class.py
@@ -2,20 +2,15 @@ from polygon import RESTClient
 client = RESTClient("ub3XXNePvsNTCCHX3Wd3wNknwrm1pzbZ")
 
 class Stock():
-    first_open = ''
-    first_high = ''
-    first_low = ''
-    first_close = ''
-    
-    sec_open = ''
-    sec_high = ''
-    sec_low = ''
-    sec_close = ''
-
     def __init__(self, tick_name, day1, day2):
         self.tick_name = tick_name
         self.day1 = day1
         self.day2 = day2
+
+        self.high = []  
+        self.low = []
+        self.close = []
+        self.open = []
 
         aggs = client.get_aggs(
             self.tick_name,
@@ -24,19 +19,14 @@ class Stock():
             self.day1,
             self.day2
         )
+            
+        for i in range(len(aggs)):
 
-        index = aggs.__len__() - 1
+            self.open.append(aggs[i].open)
+            self.high.append(aggs[i].high)
+            self.low.append(aggs[i].low)
+            self.close.append(aggs[i].close)      
 
-        Stock.first_open = aggs[0].open
-        Stock.first_high = aggs[0].high
-        Stock.first_low = aggs[0].low
-        Stock.first_close = aggs[0].close
-
-        Stock.sec_open = aggs[index].open
-        Stock.sec_high = aggs[index].high
-        Stock.sec_low = aggs[index].low
-        Stock.sec_close = aggs[index].close
-    
 
     def __str__(self):
         return f"{self.tick_name}"
