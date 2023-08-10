9fff2757c61515110849bc45b7a074d7e5c8c681
James Huang <james@Huang-Precision5550.(none)>
2023-08-09 20:10:41 -0400

    Refactored confirm input

--- confirm_input.py
+++ confirm_input.py
@@ -4,9 +4,6 @@ import csv
 import re
 
 
-# Program Description: This program will create a hashmap that will read the StockSymbol.csv
-# grab the current date, search for each of the date's stock value and confirm it exist
-
 class StockReader:
     """
     The Stock Reader class will create a hashmap that will read the Stock Symbols to
@@ -15,17 +12,15 @@ class StockReader:
     def __init__(self):
         super().__init__()
 
-    def generate_hashmap(self, user_data):
+    def generate_hashmap(self, user_data:str):
         """
         Call the function to create the hashmap
         :return: boolean value that makes sure the user_data is a valid ticker for the first window
         """
-
         my_hashmap = self.create_hashmap('StockSymbols.csv')
         return self.search(my_hashmap, user_data)
 
-
-    def user_input_dates_plus_current_date(self, year, month, day):
+    def user_input_dates_plus_current_date(self, year:str, month:str, day:str) -> bool:
         """
         grabs the current date and takes the user's input dates to be checked in the timestamp
         :param year: The first or second year input by the user
@@ -36,8 +31,8 @@ class StockReader:
         today = date.today()
         return self.time_stamp(today, year, month, day)
 
-
-    def search(self, hashmap, user_data):
+    @staticmethod
+    def search(hashmap, user_data):
         """
         Looks through the hashmap to find the company name
         :param hashmap: The hashmap that has all the company's stock abbreviation
@@ -46,64 +41,49 @@ class StockReader:
         """
         # Check if the input contains only letters and symbols
         if re.fullmatch(r'[A-Z\^]+', user_data) and len(user_data) <= 5:
-            if user_data in hashmap:
-                return True
-        else:
-            # Input is not valid, it will ask the user to try another input
-            return False
+            return user_data in hashmap
 
-
-    def time_stamp(self, today, user_year, user_month, user_day):
+    def time_stamp(self, today:date, consumer_year:str, consumer_month:str, consumer_day:str) -> bool:
         """
         Checks the timestamps to ensure all of it is valid and that it's not past the
         two-year checkmark or else it will throw an error to try again
         :param today: The current date
-        :param user_year: The first or second year of the user's input
-        :param user_month: The first or second month of the user's input
-        :param user_day: The first or second day of the user's input
+        :param consumer_year: The first or second year of the user's input
+        :param consumer_month: The first or second month of the user's input
+        :param consumer_day: The first or second day of the user's input
         :return: the boolean value to continue with the program, or else throw an error
         """
+        year = self.validate_number(consumer_year)
+        month = self.validate_number(consumer_month)
+        day = self.validate_number(consumer_day)
+        full_date = date(year, month, day)
+        delta = relativedelta.relativedelta(today, full_date)
 
-        year = int(self.validate_number(user_year))
-        month = int(self.validate_number(user_month))
-        day = int(self.validate_number(user_day))
-        fulldate = date(year, month, day)
-        delta = relativedelta.relativedelta(today, fulldate)
+        return delta.years < 2
 
-        if not delta.years < 2:
-            return False
-        else:
-            return True
-
-
-    def validate_number(self, users_value_requested):
+    @staticmethod
+    def validate_number(consumer_value:str) -> int:
         """
         Checks to ensure the value is a number rather than a string or another char
-        :param users_value_requested: a value that is either the year, month, or day
-        :return: the value since its true or false
+        :param consumer_value: a value that is either the year, month, or day
+        :return: true or false
         """
-        while True:
-            if users_value_requested.isdigit():
-                return users_value_requested
-            else:
-                return False
+        if consumer_value.isdigit():
+            return int(consumer_value)
+        else:
+            raise ValueError("The date is not valid")
 
 
-    def create_hashmap(self, filename):
+    @staticmethod
+    def create_hashmap(filename:csv) -> dict:
         """
         This will read the csv file and compute it into a hashmap for us to access
         :param filename: csv file with all the tickers(company's stock abbreviation)
         :return: a hashmap to use for the program
         """
-        my_hashmap = {}
-
         # Open the CSV file and read the data
         with open(filename, 'r') as file:
             reader = csv.reader(file)
-            for row in reader:
-                # Assign the first and second columns to the key and value, respectively
-                if len(row) == 0:
-                    continue
-                my_hashmap[row[0]] = None
+            stock_hashmap = {row[0]: None for row in reader if row and len(row) >= 1}
 
-        return my_hashmap
+        return stock_hashmap
